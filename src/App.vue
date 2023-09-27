c0167d83e71c80cbf333439791e61072ed6e6d4c
danielmegale <danim1@live.it>
2023-09-27 12:32:46 +0200

    add icons

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ export default {
 </script>
 
 <template>
-  <div class="container-xxl">
+  <div>
     <AppHomePage />
   </div>
 </template>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,4 +4,45 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <header class="d-flex justify-content-between align-items-center container-xxl my-3">
+        <div>
+            <a href="/"><img class="img-fluid" src="../../public/img/logo_navbar.png" alt=""></a>
+        </div>
+        <div>
+            <a class="user" href=""><i class="fa-solid fa-circle-user fa-2xl"></i></a>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    height: 100px;
+
+    div {
+
+        a {
+            display: block;
+            height: 100px;
+
+
+            img {
+                height: 100%;
+            }
+        }
+
+        i {
+            color: #25dd85;
+
+            &:hover {
+                color: black;
+                cursor: pointer;
+            }
+        }
+
+        .user {
+            height: 100%;
+        }
+    }
+}
+</style>
