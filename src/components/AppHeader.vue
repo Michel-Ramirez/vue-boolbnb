33a08131f114d04938b68a230063ea23ca413f72
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-10-02 12:31:07 +0200

    fix header in position sticky

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,29 +5,31 @@ export default {
 </script>
 
 <template>
-    <header class="d-flex justify-content-between align-items-center container-xxl my-2">
-        <div class="img-container d-flex align-items-center">
-            <a href="/"><img class="img-fluid" src="../../public/img/logo_navbar.png" alt=""></a>
-            <p class="ps-3 pt-3"><a href="/"
-                    class="link-secondary link-offset-2 link-underline link-underline-opacity-0 link-opacity-50-hover"><strong>Home</strong></a>
-            </p>
-        </div>
-        <div class="pe-3 action-button-group">
-            <!-- icona per il login o sing in   -->
-            <div class="user-icon dropdown ">
-                <button class="user btn dropdown-toggle no-caret" type="button" data-bs-toggle="dropdown"
-                    aria-expanded="false">
-                    <i class=" fa-regular fa-circle-user"></i>
-                </button>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Accedi</a></li>
-                    <li><a class="dropdown-item" href="#">Registrati</a></li>
-                </ul>
+    <header class="d-flex align-items-center">
+        <div class="container-xxl d-flex justify-content-between align-items-center">
+            <div class="img-container d-flex align-items-center">
+                <a href="/"><img class="img-fluid" src="../../public/img/logo_navbar.png" alt=""></a>
+                <p class="ps-3 pt-3"><a href="/"
+                        class="link-secondary link-offset-2 link-underline link-underline-opacity-0 link-opacity-50-hover"><strong>Home</strong></a>
+                </p>
             </div>
-            <!-- bottone per il login e sing in -->
-            <div class="log-button">
-                <a class="btn-custom" href="">Accedi</a>
-                <a class="btn-custom" href="">Registrati</a>
+            <div class="pe-3 action-button-group">
+                <!-- icona per il login o sing in   -->
+                <div class="user-icon dropdown ">
+                    <button class="user btn dropdown-toggle no-caret" type="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        <i class=" fa-regular fa-circle-user"></i>
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Accedi</a></li>
+                        <li><a class="dropdown-item" href="#">Registrati</a></li>
+                    </ul>
+                </div>
+                <!-- bottone per il login e sing in -->
+                <div class="log-button">
+                    <a class="btn-custom" href="">Accedi</a>
+                    <a class="btn-custom" href="">Registrati</a>
+                </div>
             </div>
         </div>
     </header>
@@ -38,6 +40,13 @@ export default {
 
 header {
     height: 70px;
+    position: sticky;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 2;
+    padding: 50px 0;
+    background-color: white;
 
     .img-container {
         height: 70px;
