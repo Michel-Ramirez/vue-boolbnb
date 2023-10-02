716e665374a9274bfa5a37735f89b3628994da1f
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-10-02 18:31:37 +0200

    add data in card

--- src/components/generals/HouseCard.vue
+++ src/components/generals/HouseCard.vue
@@ -1,33 +1,46 @@
 <script>
 export default {
-
+    props: {
+        evHouse: Object
+    }
 }
 </script>
 
 <template>
     <div class="card">
-        <img src="../../../public/img/jumbo_1.jpeg" class="card-img-top" alt="...">
+        <img :src="evHouse.photo" class="card-img-top" alt="jumbo">
         <div class="card-body">
-            <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit</h6>
-            <p class="evidence-services m-0 ">4 letti | 2 stanze | 1 bagno | 70 mq</p>
-            <p class="address m-0">Via monte video, saluzzo il merluzzo</p>
-            <div><strong>4.000 € /Notte</strong></div>
+            <h6>{{ evHouse.name }}</h6>
+            <div class="evidence-services m-0 mb-3">
+                <span>stanze {{ evHouse.total_rooms }}</span> |
+                <span>letti {{ evHouse.total_beds }}</span> |
+                <span>nagni {{ evHouse.total_bath }}</span> |
+                <span>mq {{ evHouse.mq }}</span>
+            </div>
+            <p class="address m-0">{{ evHouse.address.home_address }}</p>
+            <div><strong>{{ evHouse.night_price }} /Notte</strong></div>
         </div>
     </div>
 </template>
 
-<style lang="scss" scoped> .evidence-services {
-     font-size: 0.5rem;
-     font-weight: 600;
- }
-
- .card {
+<style lang="scss" scoped> .card {
      width: 25rem;
+
+     .card-img-top {
+         height: 350px;
+         object-fit: cover;
+     }
+
+     .evidence-services {
+         font-size: 0.6rem;
+         font-weight: 600;
+     }
+
  }
 
  @media screen and (min-width: 996px) {
      .card {
-         width: 45rem;
+         width: 25rem;
      }
  }
 </style>
