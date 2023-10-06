ce9de8b6003ca995b9ec5bbabf85a73ceed8fb7d
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-10-07 00:11:03 +0200

    add route detail card and add RouerLonk

---
+++ src/pages/HouseDetailPage.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+    <section>
+        <h1>prova</h1>
+    </section>
+</template>
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../pages/HomePage.vue";
 import SearchPage from "../pages/SearchPage.vue";
+import HouseDetailPage from "../pages/HouseDetailPage.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", name: "home", component: HomePage },
     { path: "/search", name: "searchpage", component: SearchPage },
-
+    {
+      path: "/house/:id",
+      name: "house-detail",
+      component: HouseDetailPage,
+    },
     // { path: "/not-found", name: "not-found", component: NotFoundPage },
     // { path: "/:pathMatch(.*)*", redirect: "/not-found" },
   ],
