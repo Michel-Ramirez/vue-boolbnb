c5909f53787639b96f3a82744a5e0897ab139beb
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-10-08 19:11:33 +0200

    footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+
+}
+</script>
+<template >
+    <footer>
+        <h3 class="text-center py-5">Creatori</h3>
+        <div class="container d-flex justify-content-center ">
+            <div class="row d-flex w-100">
+                <div class="col-6 d-flex flex-column align-items-center">
+                    <h5>Backend</h5>
+                    <p>Pasquale</p>
+                    <p>Alex Tanese</p>
+                    <p>Daniel</p>
+                </div>
+                <div class="col-6 d-flex flex-column align-items-center">
+                    <h5>Backend</h5>
+                    <p>Michel Ramirez</p>
+                    <p>Mario</p>
+                    <p>Daniel</p>
+                </div>
+            </div>
+        </div>
+        <div class="bottom-footer d-flex justify-content-center align-items-center">
+            <p><i>2023 clase #100 gruppo 5</i></p>
+
+        </div>
+    </footer>
+</template>
+<style lang="scss" scoped>
+footer {
+    background-color: #F7F7F7;
+}
+
+.bottom-footer {
+    height: 50px;
+}
+</style>
