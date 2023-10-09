5fa66256a9af7b660d6bebcd053c2e04727ed392
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-10-09 02:23:29 +0200

    add axios

--- src/pages/HouseDetailPage.vue
+++ src/pages/HouseDetailPage.vue
@@ -1,11 +1,29 @@
 <script>
+import axios from 'axios';
 export default {
+    data() {
+        return {
+            houseData: [],
+        }
+    },
+    methods: {
+        fetchData() {
+            const endpoint = 'http://127.0.0.1:8000/api/houses/';
+            const houseId = this.$route.params.id;
+            axios.get(endpoint + houseId).then((res) => {
+                this.houseData.push(res.data)
+            }).catch().then()
+        }
+    },
+    created() {
+        this.fetchData();
+    }
 
 }
 </script>
 <template>
     <section>
-        <h1>prova</h1>
+        {{ houseData }}
     </section>
 </template>
 <style></style>
